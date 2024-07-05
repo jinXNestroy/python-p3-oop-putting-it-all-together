8e7761b7b0ce94d8391566a8aceebebe7e89d311
DanielJoel <danpersie53@gmail.com>
2024-07-05 23:49:57 +0300

    OOP

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, condition ="old") -> None:
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    @property
+    def size(self):
+        return self._size 
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size= size
+            return self._size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "Your shoe is as good as new!"
+        if self.condition == "Your shoe is as good as new!":
+            self.condition = "New"
+        print("Your shoe is as good as new!")
+
+
+    
